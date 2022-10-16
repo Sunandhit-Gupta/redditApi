2b9c64f74bdd5d7fe2e1105aa647de12caf42c3c
Sunandhit-Gupta <sunandhitgupta@gmail.com>
2022-10-17 05:05:23 +0530

    made compatible with reddit gallery,youtube Videos embeded

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let i = 0;
 // K variabe is made so that the sharedPostId condition to search from url ,works only once and donot work after clicking next button again ------------
 let k = 0;
 let currenti = 0;
-let currentSubreddit = 'dankindianmemes'
-let subreddit = 'dankindianmemes';
+let currentSubreddit = 'india'
+let subreddit = 'india';
 var sharedPostId;
 var postId;
 var video;
+var ytUsed = false;
 
 // Variable r is used to detect whether the image content is loading for the first time or it has alread loaded once .If it is already loaded once then we use 'replaceChild' rather than 'appendChild';
 
@@ -60,10 +61,17 @@ function getVideo() {
             }
             // Changing the URL query to empty so that the URL looks clean :--------------->
             window.history.pushState({}, '', window.location.pathname);
-            isOriginalContent='';
+            isOriginalContent = '';
 
             // getting info whether content is shared from some other subreddit or original------->
             checkCrossPost = response.data.children[i].data.crosspost_parent_list;
+
+            is_gallery = response.data.children[i].data.is_gallery;
+            console.log(is_gallery);
+           
+                domain = response.data.children[i].data.domain;
+                console.log(domain);
+            
             if (checkCrossPost == undefined) {
                 isOriginalContent = true;
             } else {
@@ -79,12 +87,17 @@ function getVideo() {
                     video = response.data.children[i].data.crosspost_parent_list[0].media.reddit_video.fallback_url;
                     contentTypeVideo = true;
                 } else {
-                     imgUrl = response.data.children[i].data.crosspost_parent_list[0].url;
+                    imgUrl = response.data.children[i].data.crosspost_parent_list[0].url;
                     contentTypeVideo = false;
                 }
             }
             // ------------------------------------------------------------------------------------------------------
             if (contentTypeVideo == true) {
+
+                if (ytUsed == true) {
+                    playerDiv = document.getElementById('player');
+                    playerDiv.innerHTML = ' <!-- video added -->  <video controls ontimeupdate="timeUpdates(this)" id="ck" class="ck" preload="auto" loop>   <source id="jp" type="video/mp4" src=""></video>    <!-- audio added in video format -->  <video height="0" , width="0" controls id="au" class="au" preload="auto" loop>    <source id="ausrc" type="video/mp4" src=""></video>'
+                }
 
                 if (isOriginalContent == true) {
                     video = response.data.children[i].data.media.reddit_video.fallback_url;
@@ -140,11 +153,84 @@ function getVideo() {
                 currenti = i;
                 redditName = document.getElementById('redditName');
                 redditName.innerHTML = "r/" + currentSubreddit;
-            } else {
+
+                // making ytUsed variabe false:--------
+                ytUsed = false;
+
+            }
+            else if (domain == "youtube.com"|| domain ==  "youtu.be"){
+                ytLink = response.data.children[i].data.url;
+
+                // made ytLinkInitials to check whether youtube Url has www. in starting or not ------
+                ytLinkInitials = ytLink.slice(8,16);
+        
+                if(domain == "youtube.com"){
+                    if(ytLinkInitials == "www.yout"){
+                ytVideoId = ytLink.slice(32, 43);}
+                else if(ytLinkInitials == "youtube."){
+                    ytVideoId = ytLink.slice(28,39);
+                }
+            }
+
+                else if(domain=="youtu.be"){
+                    ytVideoId = ytLink.slice(17, 29);
+                }
+
+                let originalPostvid = response.data.children[i].data.permalink;
+                postId = response.data.children[i].data.id;
+
+
+                playerDiv = document.getElementById('player');
+                playerDiv.innerHTML = `<iframe class ="ytPlayer" id ="ytPlayer" src="https://www.youtube.com/embed/${ytVideoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+
+                // Removing Loading Icon after loading Video----------------------------------
+                loadGif.src = '';
+                loadGif.style.height = '0px';
+                loadGif.style.width = '0px';
+
+                // Linking AudioPlayback with Video---------------------------------
+
+                mut = document.getElementById('mut');
+
+                // Adding Likes and Title and Author for vdieo:--------------------------------------------
+                let vidAuthor = response.data.children[i].data.author;
+                let vidTitle = response.data.children[i].data.title;
+                let vidLike = response.data.children[i].data.ups;
+
+                elementvidTitle = document.getElementById('vidTitle');
+                elementvidAuthor = document.getElementById('vidAuthor');
+                elementvidLike = document.getElementById('vidLike');
+                elementvidAuthor.innerHTML = vidAuthor + " : ";
+                elementvidTitle.innerHTML = vidTitle;
+                elementvidLike.innerHTML = "Upvotes : " + vidLike;
+                // Adding OriginalPost link attached to Upvotes  --------------------------------------
+                vidLikeHref = document.getElementById('vidLikeHref');
+                vidLikeHref.href = "https://www.reddit.com/" + originalPostvid;
+
+
+                // Adding memeNo buttons ------>
+                memeElement = document.getElementById('memeNo');
+                memeElement.innerHTML = 'memeNo: ' + i;
+
+
+                // Changing CurrentSubreddit if subreddit is changed in this request after showing video------------
+                currentSubreddit = subreddit;
+                currenti = i;
+                redditName = document.getElementById('redditName');
+                redditName.innerHTML = "r/" + currentSubreddit;
+
+                // making ytUsed variable True:---
+                ytUsed = true;
+
+            }
+
+
+            else if (contentTypeVideo == false && is_gallery == undefined) {
 
                 if (isOriginalContent == true) {
                     imgUrl = response.data.children[i].data.url;
-                    console.log('got Url :'+ imgUrl);
+                    
                 };
                 let originalPostimg = response.data.children[i].data.permalink;
                 postId = response.data.children[i].data.id;
@@ -194,6 +280,60 @@ function getVideo() {
                 loadGif.style.width = '0px';
 
             }
+            else if (is_gallery == true) {
+
+                galleryPermalink = response.data.children[i].data.permalink;
+                let originalPostimg = response.data.children[i].data.permalink;
+                postId = response.data.children[i].data.id;
+
+                // Making html to show Images,Likes, Author and Title-------------------------------------------------------------------------------
+                imageDiv = document.getElementById('imageDiv');
+
+                imageDiv.innerHTML = `<h5 class ='imgHr'>Images will be shown Here</h5><hr>
+                <iframe id='reddit-embed' class = 'reddit-embed'src='https://www.redditmedia.com${galleryPermalink}?ref_source=embed&amp;ref=share&amp;embed=true' sandbox='allow-scripts allow-same-origin allow-popups' style='border: none;' height='300' width='640' scrolling='no'></iframe><div class='imgAaT' id='imgAaT'><span class='imgAuthor' id='imgAuthor'></span><span class='imgTitle' id='imgTitle'></span></div><div class='imgBtns'><a href='' id='imgLikeHref' class='imgLikeHref' target='_blank'><button class='imgLike' id='imgLike'></button></a></div>`
+
+                // imge = document.getElementById('imge');
+                // imge.src = imgUrl;
+
+                // Adding Likes and Title, Author for Img:--------------------------------------------
+                let imgAuthor = response.data.children[i].data.author;
+                let imgTitle = response.data.children[i].data.title;
+                let imgLike = response.data.children[i].data.ups;
+
+
+                elementimgTitle = document.getElementById('imgTitle');
+                elementimgAuthor = document.getElementById('imgAuthor');
+                elementimgLike = document.getElementById('imgLike');
+                elementimgAuthor.innerHTML = imgAuthor + " : ";
+                elementimgTitle.innerHTML = imgTitle;
+                elementimgLike.innerHTML = "Upvotes : " + imgLike;
+                // Adding OriginalPost link attached to Upvotes in img --------------------------------------
+                imgLikeHref = document.getElementById('imgLikeHref');
+                imgLikeHref.href = "https://www.reddit.com/" + originalPostimg;
+
+
+
+                // Adding memeNo buttons ------>
+                memeElement = document.getElementById('memeNo');
+                memeElement.innerHTML = 'memeNo: ' + i;
+
+
+                // Changing currentSubreddit if changed after showing image-----------------------------
+                currentSubreddit = subreddit;
+                currenti = i;
+                redditName = document.getElementById('redditName');
+                redditName.innerHTML = "r/" + currentSubreddit;
+
+                r = true;
+
+                // Removing Loading icon ---------------------------------------------------
+                loadGif.src = '';
+                loadGif.style.height = '0px';
+                loadGif.style.width = '0px';
+
+
+            }
+
 
         }
         ).catch(err => {
